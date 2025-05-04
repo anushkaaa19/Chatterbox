3e9110b489628f6b47c4c635454895164e8e7ae7
Anushka Srivastava <anushka.19252406@gmail.com>
2025-05-04 16:16:44 +0530

    setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 const app= express();
+import authRoutes from "./routes/auth.route.js";
+app.use("/ap/auth",authRoutes);
 app.listen(5001,()=>{
     console.log("Server is running");
 }
